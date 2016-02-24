36f9b6d024480628bc293d6cb8717d8894459ca9
許琇媛 <yuan198102@gmail.com>
2016-02-24 16:07:48 +0800

    Initial Commit

---
+++ GithubBomb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubBomb
+//
+//  Created by Blues on 2016/2/24.
+//  Copyright © 2016年 Blues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
